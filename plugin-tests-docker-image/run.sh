c3df399144bf52154d4233b8d6f93a6bd480ce59
realincubus <kemnitz.stefan@googlemail.com>
2016-04-21 22:18:07 +0200

    better way to test it run the container and update all repositories before testing

--- plugin-tests-docker-image/run.sh
+++ plugin-tests-docker-image/run.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# update pet
+cd ~
+cd pet
+git pull
+git submodule update
+make 
+make install
+
+#update pluto
+cd ~
+cd pluto
+git pull 
+git submodule update
+make 
+make install
+
+#update plugin
+cd ~ 
+cd llvm/tools/clang/examples/ClanPlugin
+git pull
+cd ~ 
+cd build 
+export pluto_codegen_cxx_DIR=$HOME/pet_install
+export pluto_DIR=$HOME/pluto_install
+export PATH=$PATH:$HOME/pet_install:$HOME/pluto_install
+ninja lib/ClanPlugin.so
+
+# run the tests
 cd ~ 
 cd clang_plugin_tests 
 export PATH=$HOME/pet_install/:$PATH && \
