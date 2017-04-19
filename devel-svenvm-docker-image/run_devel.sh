d0849f4e16902f11acea2060aeefbeb580959f57
incubus <lrs.incubus@googlemail.com>
2017-04-19 14:27:14 +0200

    changed container names and added a new version of svenvm to the repos

--- devel-svenvm-docker-image/run_devel.sh
+++ devel-svenvm-docker-image/run_devel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged -d --name svenvm_devel incubus/devel_svenvm /bin/bash -c 'while :; do echo "Hit CTRL+C"; sleep 1; done'
+docker run --privileged -d --name svenvm_devel incubus/devel-svenvm /bin/bash -c 'while :; do echo "Hit CTRL+C"; sleep 1; done'
