ec16bfaf131af0988bdb65d21a80868a9f506bd6
stefan kemnitz <kemnitz.stefan@gmail.com>
2017-05-08 10:37:17 +0200

    changed underscores to dashs

--- devel-ttt-docker-image/run_devel.sh
+++ devel-ttt-docker-image/run_devel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged -d --name ttt_devel incubus/devel_ttt /bin/bash -c 'while :; do echo "Hit CTRL+C"; sleep 1; done'
+docker run --privileged -d --name ttt_devel incubus/devel-ttt /bin/bash -c 'while :; do echo "Hit CTRL+C"; sleep 1; done'
