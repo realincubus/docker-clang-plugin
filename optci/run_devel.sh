0235b5273faaf691447cf06f990caccf6bfba5df
realincubus <kemnitz.stefan@googlemail.com>
2017-06-15 20:45:37 +0200

    folder

--- optci/run_devel.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --privileged -d --name optci_devel incubus/optci /bin/bash -c 'while :; do echo "Hit CTRL+C"; sleep 1; done'
--- optci/run_instance.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-docker exec -it optci_devel /bin/zsh 
