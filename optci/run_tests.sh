05e82ffeaa97fac8ca1ab424c59d372696a80fd3
stefan kemnitz <kemnitz.stefan@gmail.com>
2017-06-26 13:44:29 +0200

    reverted to old state since jenkins has more problems then before

--- optci/run_tests.sh
+++ optci/run_tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
 # TODO after proper install remove this 
 # need all paths that we also have on command line
-export PATH=$HOME/install/bin/:$PATH
-export PATH=$HOME/pet_install/:$PATH
-export PATH=$HOME/pluto_install/:$PATH
-export PATH=$HOME/ClanPlugin/:$PATH
+#export PATH=$HOME/install/bin/:$PATH
+#export PATH=$HOME/pet_install/:$PATH
+#export PATH=$HOME/pluto_install/:$PATH
+#export PATH=$HOME/ClanPlugin/:$PATH
 
 # to find the libraries
-export LD_LIBRARY_PATH=$HOME/install/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$HOME/ClanPlugin/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$HOME/pet_install/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$HOME/pluto_install/lib/:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$HOME/install/lib/:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$HOME/ClanPlugin/lib/:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$HOME/pet_install/lib/:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$HOME/pluto_install/lib/:$LD_LIBRARY_PATH
 
-export CPATH=$HOME/pluto_install/include/pluto/:$HOME/pluto_install/include/pluto_codegen_cxx:$HOME/pet_install/include/:$CPATH
+#export CPATH=$HOME/pluto_install/include/pluto/:$HOME/pluto_install/include/pluto_codegen_cxx:$HOME/pet_install/include/:$CPATH
 
 cd /root/optci/unit_tests && ctest --no-compress-output -T Test .
 rm -rf /host/*
