2b6718e39de0fdb05cb0e1a29384603be01437a1
stefan kemnitz <kemnitz.stefan@gmail.com>
2017-06-26 10:38:41 +0200

    paths for ClanPlugin.so were missing

--- optci/run_tests.sh
+++ optci/run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# need all paths that we also have on command line
+source /root/.bashrc 
 cd /root/optci/unit_tests && ctest --no-compress-output -T Test .
 rm -rf /host/*
 cp -r /root/optci/unit_tests/Testing /host/optci_results
