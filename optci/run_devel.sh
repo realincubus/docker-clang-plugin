fe9842e3cfd78bc4dfa4e10d99f42aace2073ba8
stefan kemnitz <kemnitz.stefan@gmail.com>
2017-07-12 09:31:56 +0200

    changed url to the pathogen.vim file

--- optci/run_devel.sh
+++ optci/run_devel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged -d --name optci_devel -v $PWD/share:/share incubus/optci /bin/bash -c 'while :; do echo "Hit CTRL+C"; sleep 1; done'
+docker run --privileged --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm -d --name optci_devel -v $PWD/share:/share incubus/optci /bin/bash -c 'while :; do echo "Hit CTRL+C"; sleep 1; done'
